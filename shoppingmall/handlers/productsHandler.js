4845fcfe6b2bfa46794a60d91f19486228714d18
hwiseong18 <leehs050725@naver.com>
2023-04-13 16:30:44 +0900

    2023-04-13

--- shoppingmall/handlers/productsHandler.js
+++ shoppingmall/handlers/productsHandler.js
@@ -1,7 +1,8 @@
 const pool = require('./_dbPool');
 
 const list = (req, res)=>{
-  let sql = `SELECT idproducts, name, price, registrationDate, seller From products`;
+
+  let sql = `SELECT idproducts, name, price, DATE_FORMAT(registrationDate, '%y-%m-%d') AS date, seller From products`;
   pool.query(sql, (err, rows, field)=>{
     res.render('productsList.html', { products: rows});
   })  
